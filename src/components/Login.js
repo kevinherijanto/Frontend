95de35661e8f2d2863dfdb966dc854ce58e4ca59
kevinherijanto24 <christopher.herijanto@student.ukdc.ac.id>
2025-01-21 23:01:22 +0700

    second commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  const history = useHistory();
+  const history = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
